d569613de8fa1e3144b3733b4b2630efd1b1a423
bdbaraban <375@holbertonschool.com>
2019-08-26 14:46:58 -0700

    Remove export of AppNavigator - moved to root of repo

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,4 @@
 // components barrel file
-export { default as AppNavigator } from './AppNavigator';
 export { default as BackgroundGradient } from './BackgroundGradient';
 export { default as EntypoNavigator } from './EntypoNavigator';
 export { default as TimerCountdown } from './TimerCountdown';
