e1830f9c56a2a62eb7047e65e2a000d4a999aa24
bdbaraban <375@holbertonschool.com>
2019-08-26 13:18:43 -0700

    Create barrel file exporting all components

---
+++ components/index.ts
@@ -0,0 +1,5 @@
+// components barrel file
+export { default as AppNavigator } from './AppNavigator';
+export { default as BackgroundGradient } from './BackgroundGradient';
+export { default as EntypoNavigator } from './EntypoNavigator';
+export { default as TimerCountdown } from './TimerCountdown';
