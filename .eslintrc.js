64a2a7bbaa3ba9973ca5e67cbcd599ffda54f281
bdbaraban <375@holbertonschool.com>
2019-08-25 21:13:20 -0700

    Upload eslint configuration files

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:jsx-a11y/recommended'
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-native',
+    'react-hooks',
+    'prettier',
+    'import',
+    'jsx-a11y'
+  ],
+  env: {
+    'react-native/react-native': true,
+    node: true
+  },
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true }]
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect'
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  },
+  overrides: [
+    {
+      files: ['components/screens/HomeScreen/HomeScreen.tsx'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off'
+      }
+    }
+  ]
+};
