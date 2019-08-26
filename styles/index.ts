9913b419106119626eb3be61e5bed79be7727d04
bdbaraban <375@holbertonschool.com>
2019-08-26 13:21:53 -0700

    Add trailing s to name of SettingsScreenStyle file

--- styles/index.ts
+++ styles/index.ts
@@ -3,4 +3,4 @@ export { default as AlertsScreenStyles } from './AlertsScreenStyles';
 export { default as AppStyles } from './AppStyles';
 export { default as ColorsScreenStyles } from './ColorsScreenStyles';
 export { default as HomeScreenStyles } from './HomeScreenStyles';
-export { default as SettingsScreenStyles } from './SettingsScreenStyle';
+export { default as SettingsScreenStyles } from './SettingsScreenStyles';
