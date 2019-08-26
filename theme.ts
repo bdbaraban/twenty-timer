7af44bf434d3100c9a7fd51732693b7b5aabaa86
bdbaraban <375@holbertonschool.com>
2019-08-26 12:54:57 -0700

    Move theme file to root of repo and fill out nested palette key for colors

---
+++ theme.ts
@@ -0,0 +1,36 @@
+// Global app theme
+const theme = {
+  palette: {
+    brown: {
+      solid: '#4b250c',
+      transparent: 'rgba(75, 37, 12, .5)'
+    },
+    green: {
+      solid: '#3e5641',
+      transparent: 'rgba(62, 86, 65, .5)'
+    },
+    blue: {
+      solid: '#26547c',
+      transparent: 'rgba(38, 84, 124, .5)'
+    },
+    grey: {
+      solid: '#4d5359',
+      transparent: 'rgba(77, 83, 89, .5)'
+    },
+    purple: {
+      solid: '#593f62',
+      transparent: 'rgba(89, 63, 98, .5)'
+    },
+    white: {
+      solid: '#fffbf7',
+      transparent: 'rgba(255, 251, 247, .5)'
+    },
+    textPrimary: '#fffbf7',
+    textSecondary: 'rgb(211, 211, 211)'
+  },
+  typography: {
+    fontFamily: 'Inter'
+  }
+};
+
+export default theme;
