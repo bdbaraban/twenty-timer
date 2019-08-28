5ed7e56fcc16c276e64cbda950a0bf670be0bd0a
bdbaraban <375@holbertonschool.com>
2019-08-28 12:49:39 -0700

    Update import of EntypoNavigator to use absolute path

--- __tests__/components/EntypoNavigator.test.tsx
+++ __tests__/components/EntypoNavigator.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { fireEvent, render, toJSON } from '@testing-library/react-native';
-import { EntypoNavigator } from '../../components';
+import EntypoNavigator from 'components/EntypoNavigator';
 
 test('renders and matches snapshot', (): void => {
   const { container, getByTestId } = render(
@@ -15,7 +15,7 @@ test('spreads props', (): void => {
   const props = {
     accessibilityLabel: 'entypo-navigator',
     name: 'chevron-up',
-    onPress: jest.fn((): null => null)
+    onPress: jest.fn((): void => {})
   };
   const { getByLabelText } = render(<EntypoNavigator {...props} />);
 
