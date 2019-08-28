fdb37cf4d033070a602a4246b21499a553771456
bdbaraban <375@holbertonschool.com>
2019-08-27 22:03:54 -0700

    Upload unit tests for EntypoNavigator component with corresponding snapshot

---
+++ __tests__/components/EntypoNavigator.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { fireEvent, render, toJSON } from '@testing-library/react-native';
+import { EntypoNavigator } from '../../components';
+
+test('renders and matches snapshot', (): void => {
+  const { container, getByTestId } = render(
+    <EntypoNavigator name="chevron-up" />
+  );
+
+  expect(getByTestId('entypo-navigator')).toBeTruthy();
+  expect(toJSON(container)).toMatchSnapshot();
+});
+
+test('spreads props', (): void => {
+  const props = {
+    accessibilityLabel: 'entypo-navigator',
+    name: 'chevron-up',
+    onPress: jest.fn((): null => null)
+  };
+  const { getByLabelText } = render(<EntypoNavigator {...props} />);
+
+  // Sets accessibilityLabel
+  const entypoNavigator = getByLabelText(props.accessibilityLabel);
+  expect(entypoNavigator).toBeTruthy();
+
+  // Sets onPress method
+  fireEvent.press(entypoNavigator);
+  expect(props.onPress).toHaveBeenCalled();
+});
