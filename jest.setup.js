e528da7e1eddb874485e349f71350f538548b6ce
bdbaraban <375@holbertonschool.com>
2019-08-27 21:55:08 -0700

    Load jest setup file. Initially mocks react-navigation

---
+++ jest.setup.js
@@ -0,0 +1,10 @@
+// Mock react-navigation
+jest.mock('NativeAnimatedHelper').mock('react-native-gesture-handler', () => {
+  const View = require('react-native/Libraries/Components/View/View');
+  return {
+    State: {},
+    PanGestureHandler: View,
+    BaseButton: View,
+    Directions: {}
+  };
+});
