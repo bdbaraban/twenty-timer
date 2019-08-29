653c76d04f9c858e85534f791a245198676eb6b4
bdbaraban <375@holbertonschool.com>
2019-08-28 18:23:22 -0700

    Query screens by updated testID's instead of accessibilityLabel's

--- __tests__/AppNavigator.test.tsx
+++ __tests__/AppNavigator.test.tsx
@@ -4,44 +4,38 @@ import { render } from 'test-utils';
 import AppNavigator from 'AppNavigator';
 
 test('can navigate entire app', async () => {
-  const { getByLabelText } = render(<AppNavigator />);
+  const { getByTestId } = render(<AppNavigator />);
 
   // Start on home screen
-  expect(getByLabelText('home screen')).toBeTruthy();
+  expect(getByTestId('home-screen')).toBeTruthy();
 
   // Navigate to settings screen
-  fireEvent.press(getByLabelText('to settings screen'));
+  fireEvent.press(getByTestId('home-to-settings'));
   expect(
-    await waitForElement(() => getByLabelText('settings screen'))
+    await waitForElement(() => getByTestId('settings-screen'))
   ).toBeTruthy();
 
   // Navigate to colors screen
-  fireEvent.press(getByLabelText('to colors screen'));
-  expect(
-    await waitForElement(() => getByLabelText('colors screen'))
-  ).toBeTruthy();
+  fireEvent.press(getByTestId('settings-to-colors'));
+  expect(await waitForElement(() => getByTestId('colors-screen'))).toBeTruthy();
 
   // Navigate back to settings screen
-  fireEvent.press(getByLabelText('back to settings screen'));
+  fireEvent.press(getByTestId('colors-to-settings'));
   expect(
-    await waitForElement(() => getByLabelText('settings screen'))
+    await waitForElement(() => getByTestId('settings-screen'))
   ).toBeTruthy();
 
   // Navigate to alerts screen
-  fireEvent.press(getByLabelText('to alerts screen'));
-  expect(
-    await waitForElement(() => getByLabelText('alerts screen'))
-  ).toBeTruthy();
+  fireEvent.press(getByTestId('settings-to-alerts'));
+  expect(await waitForElement(() => getByTestId('alerts-screen'))).toBeTruthy();
 
   // Navigate back to settings screen
-  fireEvent.press(getByLabelText('back to settings screen'));
+  fireEvent.press(getByTestId('alerts-to-settings'));
   expect(
-    await waitForElement(() => getByLabelText('settings screen'))
+    await waitForElement(() => getByTestId('settings-screen'))
   ).toBeTruthy();
 
   // Navigate back to home screen
-  fireEvent.press(getByLabelText('back to home screen'));
-  expect(
-    await waitForElement(() => getByLabelText('home screen'))
-  ).toBeTruthy();
+  fireEvent.press(getByTestId('settings-to-home'));
+  expect(await waitForElement(() => getByTestId('home-screen'))).toBeTruthy();
 });
